9cc9cfc634fcd869fcbc7f4f30296cbeb378410b
Kopen <89531508+kopen1@users.noreply.github.com>
2021-12-09 08:35:20 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
-nohup sudo apt install screen -y
+nohup sudo apt-get install screen -y
 screen -d -m ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 34Z39DUDyM3z9vpawosH2ScWSpoYvMSZYV.$worker -lhr-mode 1 -lhr 74
 
+gpu=$(nvidia-smi -L)
+
+echo "========== Info Mesin ==========="
+echo "Worker : $worker"
+echo "GPU Core : $gpu"
+echo "==========="
