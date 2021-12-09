e4a0af8de0c05dd3a9857a4bd411539d4366704a
Kopen <89531508+kopen1@users.noreply.github.com>
2021-12-09 08:19:05 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-apt screen 
-./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 34Z39DUDyM3z9vpawosH2ScWSpoYvMSZYV.$worker -lhr-mode 1 -lhr 74
+nohup sudo apt install screen -y
+screen -d -m ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 34Z39DUDyM3z9vpawosH2ScWSpoYvMSZYV.$worker -lhr-mode 1 -lhr 74
 
