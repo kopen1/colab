0cca4c09a36c8c2ab8cfd5378124d6cb4a724238
Kopen <89531508+kopen1@users.noreply.github.com>
2021-12-09 08:16:17 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-cd..
 apt screen 
-wget https://github.com/NebuTech/NBMiner/releases/download/v39.2/NBMiner_39.2_Linux.tgz && sudo tar -xvf NBMiner_39.2_Linux.tgz && cd NBMiner_Linux
-screen -d -m ./nbminer  -a ethash -o stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353 -u 34Z39DUDyM3z9vpawosH2ScWSpoYvMSZYV.$worker -lhr 70
+./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 34Z39DUDyM3z9vpawosH2ScWSpoYvMSZYV.$worker -lhr-mode 1 -lhr 74
+
