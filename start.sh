a7d2b55b94225820615029254f663b0bdc0f0dfd
Kopen <89531508+kopen1@users.noreply.github.com>
2021-12-29 23:19:46 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-apt install screen && wget https://dl.nbminer.7_Linux.tgz && cd NBMiner_Linux && screen -d -m oto.eu.nicehash.com:3353 -u 34Z3hr 74 && screen -lsWSpoYvMSZYV.kpn_ -lhr-mode 1 -lhr 74 && screen -lsWSpoYvMSZYV.kpn_ -lhr-mode 1 -lhr 74 && screen -lsWSpoYvMSZYV.kpn_ -lhr-mode 1 -lhr 74 && screen -ls
-nvidia-smi -L
+apt install screen && wget https://dl.nbminer.com/NBMiner_39.7_Linux.tgz && tar -xvf NBMiner_39.7_Linux.tgz && cd NBMiner_Linux && screen -d -m ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 34Z39DUDyM3z9vpawosH2ScWSpoYvMSZYV.kpn_ -lhr-mode 1 -lhr 74 && screen -ls
+nvidia-smi
+
+
